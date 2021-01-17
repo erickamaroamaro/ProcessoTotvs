a86edeb6632489b1f3ba6ce1a14e66e01cb60713
Erick <erick.amaro@fncit.com.br>
2021-01-17 14:00:21 -0300

    Initial version of cadastroPessoasTotvs generated by JHipster-6.10.4

---
+++ src/main/java/br/com/totvs/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package br.com.totvs.config.audit;
---
+++ src/main/java/br/com/totvs/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package br.com.totvs.repository;
+
+import br.com.totvs.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
